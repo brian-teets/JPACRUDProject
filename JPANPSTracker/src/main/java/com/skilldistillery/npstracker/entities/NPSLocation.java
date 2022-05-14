c99447ff93ba35105b94bd2a8718e0a0a4baee78
Brian Teets <bwt17@msn.com>
2022-05-14 09:56:14 -0600

    added columns to DB and updated forward engineering. also, updated Java entity class

--- JPANPSTracker/src/main/java/com/skilldistillery/npstracker/entities/NPSLocation.java
+++ JPANPSTracker/src/main/java/com/skilldistillery/npstracker/entities/NPSLocation.java
@@ -1,5 +1,8 @@
 package com.skilldistillery.npstracker.entities;
 
+import java.time.LocalDate;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -14,6 +17,15 @@ public class NPSLocation {
 	@GeneratedValue(strategy = GenerationType.IDENTITY) 
 	private int id;
 	private String name; 
+	@Column(name="has_visited")
+	private boolean hasVisited;
+	@Column(name="date_visited")
+	private LocalDate dateVisited;
+	private String state;
+	private String region;
+	@Column(name="nps_designation")
+	private String npsDesignation;
+	private String description;
 	
 	
 	public NPSLocation() {}
@@ -35,10 +47,70 @@ public class NPSLocation {
 		this.name = name;
 	}
 
+	public boolean isHasVisited() {
+		return hasVisited;
+	}
+
+	public void setHasVisited(boolean hasVisited) {
+		this.hasVisited = hasVisited;
+	}
+
+
+	public LocalDate getDateVisited() {
+		return dateVisited;
+	}
+
+
+	public void setDateVisited(LocalDate dateVisited) {
+		this.dateVisited = dateVisited;
+	}
+
+
+	public String getState() {
+		return state;
+	}
+
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+
+	public String getRegion() {
+		return region;
+	}
+
+
+	public void setRegion(String region) {
+		this.region = region;
+	}
+
+
+	public String getNpsDesignation() {
+		return npsDesignation;
+	}
+
+
+	public void setNpsDesignation(String npsDesignation) {
+		this.npsDesignation = npsDesignation;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
 
 	@Override
 	public String toString() {
-		return "NPSLocation [id=" + id + ", name=" + name + "]";
+		return "NPSLocation [id=" + id + ", name=" + name + ", hasVisited=" + hasVisited + ", dateVisited="
+				+ dateVisited + ", state=" + state + ", region=" + region + ", npsDesignation=" + npsDesignation
+				+ ", description=" + description + "]"; 
 	} 
 
 }
